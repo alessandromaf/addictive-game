58a73c9a124394651b32774fdd23e011369f2485
Alessandro <alessandro@myappfree.com>
2026-04-15 18:13:26 +0200

    initial commit: Tap Universe idle clicker game with PWA support

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'tap-universe-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg',
+];
+
+// Install: cache all static assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activate: remove old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: cache-first for assets, network-first for everything else
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        // Cache any new same-origin requests
+        if (e.request.url.startsWith(self.location.origin)) {
+          caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        }
+        return res;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
